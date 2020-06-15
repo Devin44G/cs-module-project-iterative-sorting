08482873b686db1bc19fe90d3d9cdfbd5b74e187
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-15 15:10:16 -0500

    Selection sorting alg and bubble sorting alg written and passing test

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,20 +5,26 @@ def selection_sort(arr):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
+        for next_index in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[next_index]:
+                smallest_index = next_index
 
         # TO-DO: swap
-        # Your code here
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    unsorted = True
 
+    while unsorted:
+        unsorted = False
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                unsorted = True
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
     return arr
 
@@ -31,11 +37,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
