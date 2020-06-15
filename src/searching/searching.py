e96ce92a14c02b10a48cc3dc35c119c4acf3c08d
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-06-15 15:37:14 -0500

    Linear and binary searches written and passing tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    begin = 0
+    end = len(arr) - 1
 
-    # Your code here
-
+    while begin <= end:
+        middle = (begin + end) // 2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            end = middle - 1
+        else:
+            begin = middle + 1
 
     return -1  # not found
